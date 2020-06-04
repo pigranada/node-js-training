dcc6482585bd24820d2ebc1e5b70e28f33950c50
Granada Ivann <ivann.granada@nokia.com>
2020-06-04 16:07:38 +0800

    Setup for heroku deployment

--- web-server/public/js/weather.js
+++ web-server/public/js/weather.js
@@ -9,20 +9,26 @@ fetch('http://puzzle.mead.io/puzzle').then((response) => {
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+messageOne.textContent = ''
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
 
-    fetch('http://localhost:3000/weather?address='+encodeURI(location)).then((response) => {
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+    fetch('/weather?address='+encodeURI(location)).then((response) => {
     response.json().then((data) => {
         if(data.error){
-            console.log('ERROR!', data.error)
+            messageOne.textContent = data.error
         }
         else{
-            console.log('Forecast:',data.forecast)
-            console.log('Location:',data.location)
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
         }
     })
 })
